95f767a90b004243daab95ec72a520ab5f2dfc18
Arold <aroldvalda@gmail.com>
2024-07-22 19:06:14 -0400

    fix test

--- test/tlconfig.lua
+++ test/tlconfig.lua
@@ -2,5 +2,6 @@ return {
 	global_env_def = "defold",
 	gen_target = "5.1",
 	gen_compat = "off",
-	include_dir = { "H:/DefoldProjects/defold-annotations/api/" }
+	-- This is for my local setup, ignore it 
+	-- include_dir = { "H:/DefoldProjects/defold-annotations-tl/api/" }
 }
